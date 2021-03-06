314f935063350c23581c8b6a802901b66f7357ae
Grigory Taran <grunder-96@mail.ru>
2021-03-07 00:17:19 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let min = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let max = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  let avg = 0;
+  let dlina = array.length;
+  for (let i = 0; i < dlina; i++) {
+    avg = avg + array[i];
+  }
+  return (avg / dlina);
 }
